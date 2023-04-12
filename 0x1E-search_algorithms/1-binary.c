7af9473ffc567e70469373767d3f94a09049c87f
Peter <kayurxpeterwise2015@gmail.com>
2023-04-12 21:05:39 +0000

    102-interpolation.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,28 +1,52 @@
 #include "search_algos.h"
-/**
-* linear_search - a function that searches for a value in an
-*array of integers using the Linear search algorithm
-* @array: is a pointer to the first element of the array to search in
-* @size: is the number of elements in array
-* @value: is the value to search for
-* Return: index OR -1
-**/
-int linear_search(int *array, size_t size, int value)
-{
-	unsigned int i;
-	int flag = 0;
 
-	if (array == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+/**
+ * print_array - prints a given a array
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ */
+void print_array(int *array, size_t size)
+{
+	size_t iter;
+
+	for (iter = 0; iter < size; iter++, array++)
 	{
-		printf("Value checked array[%u] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-		{
-			flag = 1;
-			return (i);
-		}
+		if (iter == 0)
+			printf("%d", *array);
+		else
+			printf(", %d", *array);
 	}
-	if (flag != 1)
-		return (-1);
-	return
+	printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of
+ * integers using the Binary search algorithm
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: int
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t iter;
+	int middle, compare, left, right;
+
+	for (iter = 0, left = 0, right = size - 1; size > 0; iter++)
+	{
+		middle = (int)floor((double)((left + right) / 2));
+		printf("Searching in array: ");
+		print_array(array + left, size);
+		compare = *(array + middle);
+		if (value == compare)
+			return (middle);
+		else if (value > compare)
+			left = middle + 1;
+		else if (value < compare)
+			right = middle - 1;
+		size = (right - left) + 1;
+	}
+	return (-1);
+}
