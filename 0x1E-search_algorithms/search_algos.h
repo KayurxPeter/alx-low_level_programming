a04c2fc8977e27facd33e2eba4d20a56312a6903
Peter <kayurxpeterwise2015@gmail.com>
2023-04-14 20:37:14 +0000

    1-binary.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,19 +1,12 @@
-fndef HEADER_FILE
-#define HEADER_FILE
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-/* Libraries */
+/* NECESSARY IMPORTS */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/* Prototypes */
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
 
-int b_search(int *array, int l, int r, size_t size, int value);
-void print_array(int *start, int *end);
-
-/* Structures */
 /**
  * struct listint_s - singly linked list
  *
@@ -29,15 +22,17 @@ typedef struct listint_s
 	size_t index;
 	struct listint_s *next;
 } listint_t;
+
+
 /**
- * struct skiplist_s - Singly linked list with an express line
+ * struct skiplist_s - Singly linked list with an express lane
  *
  * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
  *
- * Description: singly linked list node structure with an express line
+ * Description: singly linked list node structure with an express lane
  */
 typedef struct skiplist_s
 {
@@ -47,4 +42,16 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
-#endif
+
+ /* FUNCTION DECLARATIONS */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+#endif /* SEARCH_ALGOS */
